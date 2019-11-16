b27ed787d3beb3539f69d919c7367393a084e5f6
hp <na1212@bs.amalnet.k12.il>
2019-11-16 13:13:57 +0200

    jkllllllllll sof

--- app/src/main/java/com/example/navemenu11/Main2Activity.java
+++ app/src/main/java/com/example/navemenu11/Main2Activity.java
@@ -9,19 +9,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
+/**
+ * this activity offer you 4 options to color the screen and color it with the color that the user picked
+ */
 public class Main2Activity extends AppCompatActivity {
     LinearLayout lv2;
+    /**
+     * create the options menu with "main" list and new title "black"
+     */
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main,menu);
         menu.add(0,4,250,"black");
         return true;
     }
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         lv2= findViewById(R.id.lv2);
     }
+    /**
+     * draw the screen with the color that the user picked
+     * @param menu
+     * @return
+     */
     public boolean onOptionsItemSelected (MenuItem menu){
         int id= menu.getItemId();
         if(id==R.id.red){
@@ -39,13 +51,10 @@ public class Main2Activity extends AppCompatActivity {
         return  true;
     }
 
-
-
-
-
-
-
-
+    /**
+     * return to the first activity
+     * @param view
+     */
     public void goback(View view) {
         finish();
     }
--- app/src/main/java/com/example/navemenu11/MainActivity.java
+++ app/src/main/java/com/example/navemenu11/MainActivity.java
@@ -10,21 +10,38 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 
+/**
+ * this activity offer you 3 options to color the screen and color it with the color that the user picked
+ */
+
 public class MainActivity extends AppCompatActivity {
     LinearLayout lv;
 
     @Override
+    /**
+     * create the options menu with "main" list
+     */
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.main,menu);
 
         return true;
     }
 
+    /**
+     * @author nave avitan
+     * @param savedInstanceState
+     */
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lv= findViewById(R.id.lv);
     }
+
+    /**
+     * draw the screen with the color that the user picked
+     * @param menu
+     * @return
+     */
     public boolean onOptionsItemSelected (MenuItem menu){
         int id= menu.getItemId();
         if(id==R.id.red){
@@ -38,6 +55,11 @@ public class MainActivity extends AppCompatActivity {
         }
         return  true;
     }
+
+    /**
+     * next acticity
+     * @param view
+     */
     public void nextact(View view) {
         Intent si=new Intent(this,Main2Activity.class);
         startActivity(si);
