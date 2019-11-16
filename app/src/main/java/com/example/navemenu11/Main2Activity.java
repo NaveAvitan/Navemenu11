a3bd37d92b483a0f7f3499a687bea901e1b672f8
hp <na1212@bs.amalnet.k12.il>
2019-11-16 11:32:55 +0200

    jkllllll

--- app/src/main/java/com/example/navemenu11/Main2Activity.java
+++ app/src/main/java/com/example/navemenu11/Main2Activity.java
@@ -2,13 +2,51 @@ package com.example.navemenu11;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Main2Activity extends AppCompatActivity {
-
+    LinearLayout lv2;
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main,menu);
+        menu.add(0,4,250,"black");
+        return true;
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        lv2= findViewById(R.id.lv2);
+    }
+    public boolean onOptionsItemSelected (MenuItem menu){
+        int id= menu.getItemId();
+        if(id==R.id.red){
+            lv2.setBackgroundColor(Color.RED);
+        }
+        if(id==R.id.blue){
+            lv2.setBackgroundColor(Color.BLUE);
+        }
+        if(id==R.id.green){
+            lv2.setBackgroundColor(Color.GREEN);
+        }
+        if (id==4)
+            lv2.setBackgroundColor(Color.BLACK);
+
+        return  true;
+    }
+
+
+
+
+
+
+
+
+    public void goback(View view) {
+        finish();
     }
 }
